1d9213b5a3eb716cb233937593eff93650d3862f
Kila <kila@super-royal.com>
2021-03-25 09:17:46 +0800

    update

--- 00-bin/switch.sh
+++ 00-bin/switch.sh
@@ -1,3 +1,19 @@
-gcloud config set project kube-lab-297807
-gcloud container clusters get-credentials cluster-3  --region=asia-east1
-kubectl config current-context
+#!/bin/bash
+
+list="{common|spock|sate|game}"
+case $1 in
+        poc)
+		gcloud container clusters get-credentials poc --zone asia-southeast1-b --project h1gcp-308408
+		kubectl config current-context
+                ;;
+        test)
+		gcloud config set project kube-lab-297807
+		gcloud container clusters get-credentials cluster-3  --region=asia-east1
+		kubectl config current-context
+                ;;
+        *)
+                echo "Usage: $0 $list"
+                exit 1
+esac
+
+
