a1bcf040937a0a4f8c47d1d813be53dea56015f0
Kila <kila@super-royal.com>
2021-04-14 10:12:53 +0800

    update switch cluster bin

--- 00-bin/switch.sh
+++ 00-bin/switch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-list="{common|spock|sate|game}"
+list="{cluster setting name}"
 case $1 in
         poc)
-		gcloud container clusters get-credentials poc --zone asia-southeast1-b --project h1gcp-308408
+		gcloud container clusters get-credentials {CLUSTERNAME} --zone asia-southeast1-b --project {PROJECTNAME}
 		kubectl config current-context
                 ;;
         test)
-		gcloud config set project kube-lab-297807
-		gcloud container clusters get-credentials cluster-3  --region=asia-east1
+		gcloud config set {PROJECTNAME}
+		gcloud container clusters get-credentials {CLUSTERNAME}  --region=asia-east1
 		kubectl config current-context
                 ;;
         *)
